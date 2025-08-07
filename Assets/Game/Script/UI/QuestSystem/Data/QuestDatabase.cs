867ddbd70b685e80a8323edc5d759442c759edab
Nguyễn Vũ <70047854+nguyenvu131@users.noreply.github.com>
2025-08-07 18:46:25 +0700

    Fix

--- Assets/Game/Script/UI/QuestSystem/Data/QuestDatabase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "QuestDatabase", menuName = "Game Data/Quest Database")]
-public class QuestDatabase : ScriptableObject
-{
-    public List<QuestData> questList = new List<QuestData>();
-}
